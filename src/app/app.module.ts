3e5dbbc5f5df2dd34126dc78a2a96b3372d28453
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-23 17:22:16 -0300

    created componentts for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroComponent } from './pages/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
