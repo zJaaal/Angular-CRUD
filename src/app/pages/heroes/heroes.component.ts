62aa358ad7a6e8a28b7f62a7e8c54493c10df6df
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-23 19:36:11 -0300

    implement delete and loading

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroModel } from 'src/app/models/hero.model';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,30 @@ import { HeroesService } from 'src/app/services/heroes.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: HeroModel[] = [];
+  loading: boolean = true;
 
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
     this.heroesService
       .getHeroes()
-      .subscribe((response) => (this.heroes = [...response]));
+      .subscribe(
+        (response) => ((this.heroes = [...response]), (this.loading = false))
+      );
+  }
+
+  deleteHero(id: string) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You are going to delete a hero',
+      showConfirmButton: true,
+      showCancelButton: true,
+    }).then((action) => {
+      if (action.isConfirmed) {
+        this.heroesService.deleteHero(id).subscribe((_) => {
+          this.heroes = this.heroes.filter((hero) => hero.id != id);
+        });
+      }
+    });
   }
 }
