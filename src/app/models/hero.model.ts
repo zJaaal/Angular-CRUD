e054c7042aad30626741dd6a9391c31907dce4f9
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-23 18:14:43 -0300

    add hero model

---
+++ src/app/models/hero.model.ts
@@ -0,0 +1,12 @@
+export class HeroModel {
+  id: string;
+  name: string;
+  superPower: string;
+  alive: boolean;
+  constructor() {
+    this.id = '';
+    this.name = '';
+    this.superPower = '';
+    this.alive = true;
+  }
+}
--- src/app/pages/hero/hero.component.ts
+++ src/app/pages/hero/hero.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroModel } from 'src/app/models/hero.model';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit {
+  hero = new HeroModel();
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  save(form: NgForm) {
+    if (form.valid) return console.log('Some fields are not valid');
+    console.log(form);
+    console.log(this.hero);
+  }
+}
