e217e7bff60cc2464ae54d1f2936beb7b4333f79
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-23 17:21:50 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent,
+  },
+  {
+    path: 'hero/:id',
+    component: HeroComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'heroes',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
